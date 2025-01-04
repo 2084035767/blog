76025b72cbab06b3dc705f55665b5a58667e046f
zs <2084035767@qq.com>
2025-01-04 14:23:59 +0800

    update:C4

--- docs/.vitepress/configs/nav.ts
+++ docs/.vitepress/configs/nav.ts
@@ -2,14 +2,14 @@ import type { DefaultTheme } from 'vitepress'
 
 export const nav: DefaultTheme.Config['nav'] = [
   { text: '博文', link: '/post/', activeMatch: '^/post/' },
-  {
-    text: '编程',
-    items: [
-      { text: '文档', link: '/base/', activeMatch: '^/base/' },
-      // { text: '源码', link: '/code/', activeMatch: '^/code/' },
-      // { text: '力扣', link: '/leetcode/', activeMatch: '^/leetcode/' },
-    ],
-  },
+  // {
+  //   text: '编程',
+  //   items: [
+  //     { text: '文档', link: '/base/', activeMatch: '^/base/' },
+  //     { text: '源码', link: '/code/', activeMatch: '^/code/' },
+  //     { text: '力扣', link: '/leetcode/', activeMatch: '^/leetcode/' },
+  //   ],
+  // },
 
   { text: '友链', link: '/friends', activeMatch: '^/friends' },
   { text: '关于', link: '/about', activeMatch: '^/about' },
